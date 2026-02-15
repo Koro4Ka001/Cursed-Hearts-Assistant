212a20f7d827ccf743fb9357c648540c23ef3497
Koro4Ka001 <137198794+Koro4Ka001@users.noreply.github.com>
2026-02-15 11:55:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        toast: path.resolve(__dirname, 'public/toast.html')
+      }
+    }
+  }
 });
