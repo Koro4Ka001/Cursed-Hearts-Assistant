15b3b6c2637fea5c8e946c7d299439aea6a4d790
Koro4Ka001 <137198794+Koro4Ka001@users.noreply.github.com>
2026-02-21 21:36:23 +0300

    Create index.ts

---
+++ src/components/spell-editor/index.ts
@@ -0,0 +1,5 @@
+// src/components/spell-editor/index.ts
+
+export { SpellActionEditor } from './SpellActionEditor';
+export { SpellChainEditor } from './SpellChainEditor';
+export { SpellEditorModal } from './SpellEditorModal';
